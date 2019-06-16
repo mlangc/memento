9a7258cfa6d0b4e5e4d2e3fc1f450b75899744a7
Matthias Langer <m.langer798@gmail.com>
2019-06-16 15:02:29 +0200

    Remove unused types

--- src/main/scala/com/github/mlangc/memento/db/model/package.scala
+++ src/main/scala/com/github/mlangc/memento/db/model/package.scala
@@ -3,15 +3,6 @@ package com.github.mlangc.memento.db
 import com.softwaremill.tagging.@@
 
 package object model {
-  trait WordIdTag
-  type WordId = Long @@ WordIdTag
-
-  trait TranslationIdTag
-  type TranslationId = Long @@ TranslationIdTag
-
-  trait CheckIdTag
-  type CheckId = Long @@ CheckIdTag
-
   trait LanguageNameTag
   type LanguageName = String @@ LanguageNameTag
 }

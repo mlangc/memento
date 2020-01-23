633044e79548444be6655d5bf8132f2e3571ee43
Matthias Langer <m.langer798@gmail.com>
2020-01-23 07:39:58 +0100

    Cosmetics

--- src/main/scala/com/github/mlangc/memento/util/VersionInfo.scala
+++ src/main/scala/com/github/mlangc/memento/util/VersionInfo.scala
@@ -5,7 +5,7 @@ import com.github.mlangc.memento.BuildInfo
 object VersionInfo {
   def render: String = {
     val bi = BuildInfo
-    val head = if (!bi.dirty) bi.head else s" uncommitted changes based on ${bi.head} build at ${bi.builtAtString}"
+    val head = if (!bi.dirty) bi.head else s"uncommitted changes based on ${bi.head} build at ${bi.builtAtString}"
     s"${bi.name}-${bi.version} (built from $head)"
   }
 }
